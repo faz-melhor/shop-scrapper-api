1d98c0389a9e218244fa230cb1cc7f9b5458d641
Diogo Augusto <diogo.augustoo@gmail.com>
2020-01-07 00:00:52 -0300

    Removed unused libs

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-from scrapper import hardmob
-from utils import data
-from utils import utils
+# from scrapper import hardmob
+# from utils import utils
 
-raw_data = hardmob.scrap_hardmob()
+# raw_data = hardmob.scrap_hardmob()
 
 # Printing the data
 # print_json(raw_data)
 
 # Temporarily saving in local file
-utils.write_file(raw_data, 'data.json')
+# utils.write_file(raw_data, 'data.json')
